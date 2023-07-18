6691a5163068ce5b9e11b68adebed190208a6c73
Rohit Goswami <rog32@hi.is>
2023-07-18 14:58:48 +0000

    MAINT: Use current fprettify settings

--- app/conv.f90
+++ app/conv.f90
@@ -17,24 +17,24 @@ program conv
 !       0, 1: 3rd parameter = Ne (Number of elements)
 !       2   : 3rd parameter = p  (Polynomial order)
 
-    use graphs, only: run_convergence
-    implicit none
+use graphs, only: run_convergence
+implicit none
 
-    integer :: dirac_int, study_type, p_or_Ne
-    character(len=128) :: arg
+integer :: dirac_int, study_type, p_or_Ne
+character(len=128) :: arg
 
-    if (command_argument_count() /= 3) then
-        print*,"./conv <study_type> <equation> <Ne/p>"
-        error stop "Must supply 3 arguments"
-    end if
+if (command_argument_count() /= 3) then
+    print*,"./conv <study_type> <equation> <Ne/p>"
+    error stop "Must supply 3 arguments"
+end if
 
-    call get_command_argument(1, arg)
-    read (arg, '(i4)') study_type
-    call get_command_argument(2, arg)
-    read (arg, '(i4)') dirac_int
-    call get_command_argument(3, arg)
-    read (arg, '(i4)') p_or_Ne
+call get_command_argument(1, arg)
+read (arg, '(i4)') study_type
+call get_command_argument(2, arg)
+read (arg, '(i4)') dirac_int
+call get_command_argument(3, arg)
+read (arg, '(i4)') p_or_Ne
 
-    call run_convergence(study_type, dirac_int, p_or_Ne, ".")
+call run_convergence(study_type, dirac_int, p_or_Ne, ".")
 
 end program
