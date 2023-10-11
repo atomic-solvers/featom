193fc6f23d2c687dffeb9b7b95fac2f3834f2ab6
Ondřej Čertík <ondrej@certik.us>
2023-10-11 12:03:57 +0200

    Initialize focc

--- src/schroed_dirac_solver.f90
+++ src/schroed_dirac_solver.f90
@@ -111,21 +111,42 @@ contains
             end do
         end do
     else
-        ! TODO: allocate focc, focc_idx
         Lmin2 = -6
         Lmax = 5
+        allocate(Vin(size(xq,1), size(xq,2)))
+        Vin = 0
         allocate(xiq_gj(size(xiq1),Lmin:Lmax))
         allocate(wtq_gj(size(wtq1),Lmin:Lmax))
-        do kappa = -6, 5
+
+        ! Initialize focc and focc_idx
+        allocate(focc(max(Lmax,abs(Lmin2))+1,Lmin2:Lmax))
+        allocate(focc_idx(max(Lmax,abs(Lmin2))+1,Lmin2:Lmax))
+        focc = 0
+        focc_idx = 0
+        do kappa = Lmin2, Lmax
             if (kappa == 0) cycle
             if (alpha_j(kappa) > -1) then
                 call gauss_jacobi_gw(Nq, 0.0_dp, alpha_j(kappa), xiq1, wtq1)
                 xiq_gj(:,kappa) = xiq1
                 wtq_gj(:,kappa) = wtq1
             end if
+            if (kappa < 0) then
+                l = -kappa-1
+            else
+                l = kappa
+            end if
+            do k = 1, 7-l
+                ind = (kappa+1)*(kappa+1)+(k-1)*(2*kappa+1)+(k-1)*(k-2)
+                if (kappa < 0) then
+                    ind = ind + 2*k-2+(4*k-2)*(-1-kappa)
+                end if
+                if (kappa == -1) then
+                    ind = ind + 1
+                end if
+                focc_idx(k,kappa) = ind
+                focc(k,kappa) = 1
+            end do
         end do
-        allocate(Vin(size(xq,1), size(xq,2)))
-        Vin = 0
         call solve_dirac_eigenproblem(Nb, Nq, Lmin2, Lmax, alpha, alpha_j, xe, xiq_gj, &
             xq, xq1, wtq_gj, V, Z, Vin, D, S2, H, lam2, rho, rho1, .false., fullc, &
             ib, in, idx, lam_tmp, uq, wtq, xin, xiq, focc, focc_idx, lam, xq)
